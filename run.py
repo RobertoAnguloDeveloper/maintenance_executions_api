055ed449f016b58426bc9a7392aa66523c5e653c
RobertoAnguloDeveloper <rangulot@outlook.com>
2025-03-09 21:17:05 -0500

    HTTPS Bypass failed, process to revert

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 # run.py
 from app import create_app
-from flask_talisman import Talisman
+#from flask_talisman import Talisman
 
 app = create_app()
-Talisman(app, force_https=True)  # Force HTTPS
+#Talisman(app, force_https=True)  # Force HTTPS
 
 if __name__ == '__main__':
-    app.run(debug=True, ssl_context='adhoc')
-    #app.run(debug=True)
+    #app.run(debug=True, ssl_context='adhoc')
+    app.run(debug=True)
